8ddfa8d22128cab68e56b2473773719608bb58bb
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2021-12-19 23:12:51 -0500

    add favicon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react'
+import { Layout } from 'antd'
 import logo from './assets/logo/logo.svg'
 import './App.css'
 
+const { Content } = Layout
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="texto">
-          Bienvenido a <code>01a01</code>, desarrollandose con:
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React
-        </a>
-      </header>
-    </div>
+    <>
+      <Layout>
+        {/* <Header>header</Header> */}
+        <Layout>
+          {/* <Sider>left sidebar</Sider> */}
+          <Content className="main-content">
+            <center>
+              <div className="main-logo">
+                <img src={logo} alt="logo" />
+              </div>
+              ¡En desarrollo!
+            </center>
+            <br />
+            <div className="texto-principal">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sit amet
+              varius ante. Pellentesque pretium, risus eu dictum pulvinar, neque nibh
+              laoreet mi, aliquet gravida urna sem quis ligula. Nullam quis dui
+              malesuada, facilisis nibh eu, volutpat sapien. Nulla tempus ornare
+              purus, vel lobortis mi malesuada maximus.
+            </div>
+          </Content>
+        </Layout>
+        {/* <Footer>footer</Footer> */}
+      </Layout>
+    </>
   )
 }
 
