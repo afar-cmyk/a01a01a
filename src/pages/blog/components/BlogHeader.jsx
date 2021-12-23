8acc68f16ef50366097ee9a864fb7c80e86a7f2e
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2021-12-22 23:56:36 -0500

    fix minor bugs

--- src/pages/blog/components/BlogHeader.jsx
+++ src/pages/blog/components/BlogHeader.jsx
@@ -5,28 +5,27 @@ export default function BlogHeader(props) {
   // propiedades del componente
   const { title, date } = props
 
-  // Estilos del componente
-  const titleContainer = {
-    textAlign: 'justify',
-    textAlignLast: 'left'
-  }
-
-  const dateContainer = {
-    fontSize: '1rem'
-  }
-
   return (
     <>
       <div style={{ ...titleContainer }}>
-        <span style={{ dateContainer }}>{date}</span>
+        <span style={{ ...dateContainer }}>{date}</span>
         <h1>{title}</h1>
       </div>
     </>
   )
 }
 
-// PropTypes para definir los props del componente
+// Estilos del componente
+const titleContainer = {
+  textAlign: 'justify',
+  textAlignLast: 'left'
+}
 
+const dateContainer = {
+  fontSize: '1rem'
+}
+
+// PropTypes para definir los props del componente
 BlogHeader.propTypes = {
   title: PropTypes.string,
   date: PropTypes.string
---
+++ src/pages/blog/components/BlogParagraph.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function BlogParagraph(children) {
+  // propiedades del componente
+
+  return (
+    <>
+      <span>
+        <p>{children}</p>
+      </span>
+    </>
+  )
+}
+
+// Estilos del componente
+
+// PropTypes para definir los props del componente
